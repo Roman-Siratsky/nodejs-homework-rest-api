5770ffc4f7b597ea0e0a3763b0203f69cdcb7b20
romans <siratsky.roman@gmail.com>
2021-11-27 18:38:20 +0200

    fixed mail service

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,21 +4,12 @@ const usersController = require('../../controllers/usersController.js')
 const authMiddleware = require('../../middlewares/authMiddleware.js')
 const uploadMiddleware = require('../../middlewares/uploadMiddleware')
 
-router.post(
-  '/signup',
-  // uploadMiddleware.single('avatar'),
-  usersController.signup
-)
+router.post('/signup', usersController.signup)
 router.post('/login', usersController.login)
 router.post('/logout', usersController.logout)
 router.get('/activate/:link', usersController.activate)
 router.get('/refresh', usersController.refresh)
 router.get('/current', authMiddleware, usersController.getCurrentUser)
-router.patch(
-  '/avatars',
-  authMiddleware,
-  uploadMiddleware.single('avatar'),
-  usersController.avatars
-)
+router.patch('/avatars',authMiddleware,uploadMiddleware.single('avatar'),usersController.avatars)
 
 module.exports = router
